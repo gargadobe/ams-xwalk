c23bada51562fa168c6a878df67d553e9a75d96c
arunattri26 <5784970+arunattri26@users.noreply.github.com>
2025-05-26 14:14:22 +0530

    Update form-editor-support.js

--- scripts/form-editor-support.js
+++ scripts/form-editor-support.js
@@ -351,21 +351,21 @@ const observer = new MutationObserver(instrumentForms);
 observer.observe(document, { childList: true, subtree: true, attributeFilter: ['form'] });
 loadCSS(`${window.hlx.codeBasePath}/scripts/form-editor-support.css`);
 
-function enableRuleEditorExtension() {
-  const head = document.getElementsByTagName('head')[0];
-  const meta = document.createElement('meta');
-  meta.name = 'urn:adobe:aue:config:extensions';
-  // const params = new URLSearchParams(window.location.hash.substring(1)); // Remove the leading '#' character
-  const params = new URLSearchParams(window.location.search);
-  console.log('Rule editor extension Params: ', params);
-  const version = params.get('livecycle-ruleeditor-ui-service_version');
-  const domain = params.get('livecycle-ruleeditor-ui-service_domain') || 'https://experience.adobe.com';
-  if (version) {
-    meta.content = `${domain}/solutions/livecycle-ruleeditor-ui-service/static-assets/resources/universal_editor.html?livecycle-ruleeditor-ui-service_version=${version}`;
-  } else {
-    meta.content = `${domain}/solutions/livecycle-ruleeditor-ui-service/static-assets/resources/universal_editor.html`;
-  }
-  console.log('Adding meta tag for rule editor extension: ', meta.content);
-  head.appendChild(meta);
-}
+// function enableRuleEditorExtension() {
+//   const head = document.getElementsByTagName('head')[0];
+//   const meta = document.createElement('meta');
+//   meta.name = 'urn:adobe:aue:config:extensions';
+//   // const params = new URLSearchParams(window.location.hash.substring(1)); // Remove the leading '#' character
+//   const params = new URLSearchParams(window.location.search);
+//   console.log('Rule editor extension Params: ', params);
+//   const version = params.get('livecycle-ruleeditor-ui-service_version');
+//   const domain = params.get('livecycle-ruleeditor-ui-service_domain') || 'https://experience.adobe.com';
+//   if (version) {
+//     meta.content = `${domain}/solutions/livecycle-ruleeditor-ui-service/static-assets/resources/universal_editor.html?livecycle-ruleeditor-ui-service_version=${version}`;
+//   } else {
+//     meta.content = `${domain}/solutions/livecycle-ruleeditor-ui-service/static-assets/resources/universal_editor.html`;
+//   }
+//   console.log('Adding meta tag for rule editor extension: ', meta.content);
+//   head.appendChild(meta);
+// }
 //enableRuleEditorExtension();
